028826f1fe0934d6495cbacfe707ba99a6c03173
arinko0025 <b5069@oic.jp>
2016-11-24 22:28:06 +0900

    seed 途中

---
+++ database/seeds/users.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class users extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->delete();
+      //faker使う。普通に使う場合と同じ。
+      $faker = Faker\Factory::create('ja_JP');
+      for($i=1; $i<=15; $i++)
+      {
+        DB::table('users')->insert([
+            'id'=>$i,
+            'name'=>$faker->name(),
+            'email'=>$i.$faker->email(),
+            'password'=>$faker->password(),
+            'sex'=>$faker->boolean(),
+            'address'=>$faker->address(),
+            'tel'=>$faker->randomDigit(),
+        ]);
+      }
+    }
+}
