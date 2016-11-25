7c8a0a25c0c7b2a6476af6bf6c7c31d03659b124
arinko0025 <b5069@oic.jp>
2016-11-26 00:01:44 +0900

    seed書いたよ

--- database/seeds/CATEGORY.php
+++ database/seeds/CATEGORY.php
@@ -11,6 +11,29 @@ class CATEGORY extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('CATEGORY')->insert([
+         'id'=>1,
+         'name'=>'小物入れ',
+       ]);
+      DB::table('CATEGORY')->insert([
+         'id'=>2,
+         'name'=>'マスキングテープ',
+      ]);
+      DB::table('CATEGORY')->insert([
+         'id'=>3,
+         'name'=>'マグカップ',
+      ]);
+      DB::table('CATEGORY')->insert([
+         'id'=>4,
+         'name'=>'テーブル',
+      ]);
+      DB::table('CATEGORY')->insert([
+         'id'=>5,
+         'name'=>'キャビネット',
+      ]);
+      DB::table('CATEGORY')->insert([
+         'id'=>6,
+         'name'=>'置き時計',
+      ]);
     }
 }
